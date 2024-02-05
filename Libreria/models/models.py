780a0d21b47ac61055299c80a2f18dca832271a1
Lucas Federici <Luckefederici1@gmail.com>
2024-02-05 20:48:52 -0300

    creacion de models, configuracion de app y registro de models

--- Libreria/models/models.py
+++ Libreria/models/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Libro(models.Model):
+    titulo = models.CharField(max_length = 50)
+    autor = models.CharField(max_length = 50)
+    genero = models.CharField(max_length = 60)
+    publicado = models.DateField()
+
+    #metodo descriptivo
+    def __str__(self):
+        return self.titulo +'-'+ self.autor + '-' + self.publicado
+    
+    #ordenar
+    class Meta:
+        ordering = ['-titulo']
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length = 100)
+    apellido = models.CharField(max_length = 100)
+    nacimiento = models.DateField()
+    
+    def __str__(self):
+        return self.nombre  +'-' + self.apellido
+
+
+class Clientes(models.Model):
+    nombre = models.CharField(max_length = 50)
+    apellido =models.CharField(max_length = 50)
+    email = models.EmailField()
+
